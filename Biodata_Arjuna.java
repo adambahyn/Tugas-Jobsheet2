51840a5d612b4a087a1da72106902d6318f6beb8
Adammm <adambahymaulana7@gmail.com>
2024-09-16 16:45:24 +0700

    Menambah file biodata arjuna

---
+++ Biodata_Arjuna.java
@@ -0,0 +1,12 @@
+public class Biodata_Arjuna{
+    public static void main(String[] args){
+
+        System.out.println("Nama                        : Arjuna Julian Faeyza\r");
+        System.out.println("Tempat, Tanggal dan Lahir   : Malang, 11 Mei 2013\r");
+        System.out.println("Alamat                      : JL. Kemirahan 2F no.9, Kota Malang\r");
+        System.out.println("No. Telp                    : 082332755867\r");
+        System.out.println("Email                       : chilaaaaaaaaaaa@gmail.com\r");
+        System.out.println("Hobi                        : Bermain Gim\r");
+
+    }
+}
